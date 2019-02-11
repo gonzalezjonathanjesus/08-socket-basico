5367eaf9926e6037a828860d6b6f534811d9233c
gonzalezjonathanjesus <gonzalezjonathanjesus@gmail.com>
2019-02-11 00:10:42 -0300

    Listen and send messages server to client - client to server

--- server/server.js
+++ server/server.js
@@ -1,17 +1,40 @@
 const express = require('express');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const path = require('path');
 
 const app = express();
+let server = http.createServer(app);
 
 const publicPath = path.resolve(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
-app.use(express.static(publicPath));
+app.use(express.static(publicPath)); // Middleware para habilitar la carpeta pública.
+
+// IO = Esta es la comunicación del backend
+let io = socketIO(server);
+
+io.on('connection', (client) => {
+
+    console.log('Usuario conectado');
+
+    client.on('disconnect', () => {
+        console.log('Usuario desconectado');
+    });
+
+    // Escuchar el cliente
+
+    client.on('enviarMensaje', (message) => {
+        client.emit('enviarMensaje', {
+            user: 'Administrador',
+            message: 'Bienvenido a esta aplicación ' + message.user
+        })
+    });
+});
 
 
-
-app.listen(port, (err) => {
+server.listen(port, (err) => {
 
     if (err) throw new Error(err);
 
